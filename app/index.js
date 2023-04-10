cbd8aab939751babe771975d342ba32fd5054f8a
Sourav Majumder <souravmajumderk99@gmail.com>
2023-04-10 18:58:09 +0600

    that's it for today i hope

---
+++ app/index.js
@@ -0,0 +1,30 @@
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+// import tw from "tailwind-react-native-classnames";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimenstion="60%" />
+          ),
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
